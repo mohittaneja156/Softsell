7608ae6e39e5c7edf94e1b7787171abfe5f84afd
Mohit Taneja <mohittaneja156@gmail.com>
2025-05-17 15:04:11 +0530

    upadated api check

--- src/components/ChatWidget.jsx
+++ src/components/ChatWidget.jsx
@@ -3,7 +3,6 @@ import { FaComments, FaTimes, FaPaperPlane } from 'react-icons/fa';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
 
